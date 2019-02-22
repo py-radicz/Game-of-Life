c0b6f9f9776fd2cdc17024540c14ba1b566a99a4
Radim Keske <radim.keske@agroup.cz>
2019-02-22 15:41:39 +0100

    refactored after two years - but the code is slower - todo: make it performant

--- game.py
+++ game.py
@@ -1,91 +1,108 @@
+from pygame.locals import QUIT, Rect
+import numpy as np
+import random
 import pygame
 import sys
-import random
-from pygame.locals import *
-
-FPS = 10
-WINDOW = 500
-CELLSIZE = 5
-PADDING = (WINDOW // CELLSIZE) - 1
-assert WINDOW % CELLSIZE == 0, "win size must be a multiple of cell"
 
 
-class Board():
+class Board:
+    def __init__(self, grid_size, cell_size):
+        self.FPS = 10
+        self.dead_color = (20, 120, 20)
+        self.alive_color = (255, 255, 255)
+        self.grid_size = grid_size
+        self.cell_size = cell_size
+        self.window_size = (self.grid_size * self.cell_size) - 1
+        print(self.window_size)
 
-	def __init__(self):
+        self.grid = {
+            (x * self.cell_size, y * self.cell_size): random.choice(
+                [self.dead_color, self.alive_color]
+            )
+            for x in range(self.grid_size)
+            for y in range(self.grid_size)
+        }
 
-		pygame.init()
-		pygame.display.set_caption('Game of Life')
-		self.screen = pygame.display.set_mode((WINDOW + PADDING, WINDOW + PADDING))
-		self.grid = [[0] * (WINDOW // CELLSIZE) for i in range(WINDOW // CELLSIZE)]
+        pygame.init()
+        pygame.display.set_caption("Game of Life")
+        self.screen = pygame.display.set_mode((self.window_size, self.window_size))
 
-	def draw(self):
-		for i, x in enumerate(range(0, WINDOW + PADDING, CELLSIZE + 1)):
-			for j, y in enumerate(range(0, WINDOW + PADDING, CELLSIZE + 1)):
+    def __draw(self):
+        for coords, color in self.grid.items():
+            pygame.draw.rect(
+                self.screen,
+                color,
+                Rect(coords, (self.cell_size - 1, self.cell_size - 1)),
+            )
 
-				if self.grid[i][j] == 0:
-					color = (20, 120, 20)
-				else:
-					color = (255, 255, 255)
+    def __count_neighbors(self, coords):
+        coords = np.array([coords] * 8)
+        directions = np.array(
+            [
+                (0, -self.cell_size),
+                (self.cell_size, -self.cell_size),
+                (-self.cell_size, -self.cell_size),
+                (0, self.cell_size),
+                (self.cell_size, self.cell_size),
+                (-self.cell_size, self.cell_size),
+                (self.cell_size, 0),
+                (-self.cell_size, 0),
+            ]
+        )
 
-				pygame.draw.rect(self.screen, color, Rect((x, y), (CELLSIZE, CELLSIZE)))
+        neighbors = []
+        for direction in (coords + directions).tolist():
+            if (
+                len(
+                    [
+                        x
+                        for x in direction
+                        if x >= 0 and x < self.cell_size * self.grid_size
+                    ]
+                )
+                < 2
+            ):
+                neighbors.append(self.dead_color)
+            else:
+                neighbors.append(self.grid[tuple(direction)])
 
-	def randomize(self):
-		for i in range(1, (WINDOW // CELLSIZE) - 1):
-			for j in range(1, (WINDOW // CELLSIZE) - 1):
+        return neighbors.count(self.alive_color)
 
-				if random.randint(0, 100) < 50:
-					self.grid[i][j] = 1
+    def __apply_rules(self):
+        new_grid = {}
+        for coords in self.grid:
+            if self.grid[coords] == self.dead_color:
+                n = self.__count_neighbors(coords)
+                if n == 3:
+                    new_grid[coords] = self.alive_color
+                else:
+                    new_grid[coords] = self.dead_color
 
-	def rules(self):
-		new_grid = [[0] * (WINDOW // CELLSIZE) for i in range(WINDOW // CELLSIZE)]
+            if self.grid[coords] == self.alive_color:
+                n = self.__count_neighbors(coords)
+                if n < 2:
+                    new_grid[coords] = self.dead_color
+                elif n > 3:
+                    new_grid[coords] = self.dead_color
+                else:
+                    new_grid[coords] = self.alive_color
 
-		for i in range(1, (WINDOW // CELLSIZE) - 1):
-			for j in range(1, (WINDOW // CELLSIZE) - 1):
+        self.grid = new_grid
 
-				if self.grid[i][j] == 0:
+    def create_life(self):
+        fps_clock = pygame.time.Clock()
 
-					neighbors = self.grid[i + 1][j] + self.grid[i - 1][j] + \
-						self.grid[i][j + 1] + self.grid[i][j - 1] + self.grid[i - 1][j - 1]\
-						+ self.grid[i + 1][j + 1] + self.grid[i + 1][j - 1]\
-						+ self.grid[i - 1][j + 1]
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-					if neighbors == 3:
-						new_grid[i][j] = 1
-					else:
-						new_grid[i][j] = 0
-
-				if self.grid[i][j] == 1:
-
-					neighbors = self.grid[i + 1][j] + self.grid[i - 1][j] + \
-						self.grid[i][j + 1] + self.grid[i][j - 1] + self.grid[i - 1][j - 1]\
-						+ self.grid[i + 1][j + 1] + self.grid[i + 1][j - 1]\
-						+ self.grid[i - 1][j + 1]
-
-					if neighbors < 2:
-						new_grid[i][j] = 0
-					elif neighbors > 3:
-						new_grid[i][j] = 0
-					else:
-						new_grid[i][j] = 1
-
-		self.grid = new_grid
-
-	def mainloop(self):
-		fps_clock = pygame.time.Clock()
-
-		while True:
-			for event in pygame.event.get():
-				if event.type == QUIT:
-					pygame.quit()
-					sys.exit()
-
-			self.rules()
-			self.draw()
-			pygame.display.update()
-			fps_clock.tick(FPS)
+            self.__apply_rules()
+            self.__draw()
+            pygame.display.update()
+            fps_clock.tick(self.FPS)
 
 
-board = Board()
-board.randomize()
-board.mainloop()
+if __name__ == "__main__":
+    Board(grid_size=100, cell_size=6).create_life()
